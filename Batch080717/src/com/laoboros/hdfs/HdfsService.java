f9d98400582706a514e7be1ea5967fdcfd0c6d16
svaduka <sainagaraju.vaduka@gmail.com>
2017-08-20 09:10:23 +0530

    Created HDFSService
    Step-1: Created class extends Configured implements Tool
    to have ToolRunner.run
    
    Step-2: Beautification of code

---
+++ Batch080717/src/com/laoboros/hdfs/HdfsService.java
@@ -0,0 +1,19 @@
+package com.laoboros.hdfs;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+
+public class HdfsService extends Configured implements Tool {
+	
+	public static void main(String[] args) {
+
+	}
+
+	@Override
+	public int run(String[] args) throws Exception {
+		return 0;
+	}
+
+	
+
+}
